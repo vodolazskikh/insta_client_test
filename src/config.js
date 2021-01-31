503cc1f3b0208c5e2772dda288f67d786e952e3c
a.vodolazskikh <a.vodolazskikh@2gis.ru>
2021-01-31 15:50:54 +0700

    fix

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ export const config = {
   instagramApi: "https://api.instagram.com/",
   appId: "476292603759149",
   appKey: "f612bebf6feeff0d08d945433cfcce61",
-  api: "https://insta-client-test.vercel.app/",
+  api: "https://instaapigram.herokuapp.com/",
 };
